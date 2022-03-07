e55dfa03a1316045276a0f8b45d65649ec569b66
Jeffrey Shen <jeffdshen@gmail.com>
2022-03-07 15:00:04 -0800

    Add more to top level module

--- feedback/__init__.py
+++ feedback/__init__.py
@@ -1,9 +1,16 @@
 from .utils import set_seed
 
-from .datasets import get_dfs, get_block_dataset, FeedbackDataset, score, max_labels
+from .datasets import (
+    get_dfs,
+    get_block_dataset,
+    FeedbackDataset,
+    score,
+    max_labels,
+    pred_to_words,
+)
 
 from .models import FeedbackModel, get_linear_warmup_power_decay_scheduler
 
 from .stats import EMAMeter, AverageMeter, MinMeter, F1Meter, MaxMeter, F1EMAMeter
 
-from .train import run
+from .train import run, to_device
