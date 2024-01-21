763698bb75db3ea8385fa8bd739233d3271dcfeb
Jeffrey Shen <jeffdshen@gmail.com>
2024-01-21 04:26:17 -0800

    compile model, infer

--- daigt/__init__.py
+++ daigt/__init__.py
@@ -10,3 +10,5 @@ from .models import DaigtModel, get_linear_warmup_power_decay_scheduler, split_b
 from .stats import EMAMeter, AverageMeter, SlidingAucRocMeter, TotalAucRocMeter, MinMeter, MaxMeter
 
 from .train import run, to_device, NoopWandB, get_dataset_splits_for_training
+
+from .infer import predict_fold, get_submission, ensemble
